f5462ec1cb0cbaa20d52a960cc0b7102229913f8
Chiranthstack <chiranth.s2005@gmail.com>
2026-01-11 22:08:59 +0530

    Time: 1 ms (99.72%) | Memory: 62.1 MB (34.35%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
@@ -1,31 +1,19 @@
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
-     
-    int minLenWindow = Integer.MAX_VALUE;
-    int currentSum = 0;
+        int left = 0;
+        int sum = 0;
+        int minLen = Integer.MAX_VALUE;
 
-    // Start 2 pointers sliding window
-    int low = 0;
-    int high = 0;
-    while(high < nums.length) {
+        for (int right = 0; right < nums.length; right++) {
+            sum += nums[right];   // add box
 
-      // Find the current sum and increase window size
-      currentSum += nums[high];
-      high++;
+            while (sum >= target) {
+                minLen = Math.min(minLen, right - left + 1);
+                sum -= nums[left]; // remove box
+                left++;            // shrink window
+            }
+        }
 
-      // Try to reduce the window size
-      while (currentSum >= target) {
-
-        int currentWindowSize = high - low;
-
-        // Update minimum length of window
-        minLenWindow = Math.min(minLenWindow, currentWindowSize);
-
-        currentSum -= nums[low];
-        low++;
-      }
-    }
-
-    return minLenWindow == Integer.MAX_VALUE ? 0 : minLenWindow;   
+        return minLen == Integer.MAX_VALUE ? 0 : minLen;
     }
 }
