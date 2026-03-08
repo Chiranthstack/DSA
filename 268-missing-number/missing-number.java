b750114eff822b9d76fc94572d8c533b02106846
Chiranthstack <chiranth.s2005@gmail.com>
2026-03-08 10:39:40 +0530

    Time: 0 ms (100.00%) | Memory: 47.2 MB (91.53%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,20 +1,13 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        //we use XOR approach as XOR is commutative and each xor will get mapped
-         int allXOR = 0;
-
-    // XOR all numbers in range [0, n]
-    for (int i = 0; i <= nums.length; i++) {
-      allXOR = allXOR ^ i;
-    }
-
-    // XOR all numbers in the given array
-    for (int num : nums) {
-      allXOR = allXOR ^ num;
-    }
-
-    // The missing number
-    return allXOR;
+        int xor=0;
+        for(int i=0;i<=nums.length;i++){
+            xor=xor^i;
+        }
         
+        for(int num:nums){
+            xor=xor^num;
+        }
+        return xor;
     }
 }
