535893043a138e72f380834aea45163a6f7df027
Chiranthstack <chiranth.s2005@gmail.com>
2026-03-22 00:16:54 +0530

    Time: 26 ms (25.47%) | Memory: 76.5 MB (64.55%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.java
+++ 219-contains-duplicate-ii/contains-duplicate-ii.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        Set <Integer> set = new HashSet<>();
+        Set<Integer> set = new HashSet<>();
         for(int i=0;i<nums.length;i++){
             if(set.contains(nums[i]))return true;
             set.add(nums[i]);
@@ -9,5 +9,6 @@ class Solution {
             }
         }
         return false;
+        
     }
 }
