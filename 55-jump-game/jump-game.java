c6cf7592d1c8a2a5e2d46e5cfaf51b9578e4e47c
Chiranthstack <chiranth.s2005@gmail.com>
2026-03-24 22:17:56 +0530

    Time: 2 ms (85.53%) | Memory: 47.8 MB (73.96%) - LeetSync

---
+++ 55-jump-game/jump-game.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int finalPosition=nums.length-1;
+        for(int index=nums.length-2;index>=0;index--){
+            if(index+nums[index]>=finalPosition){
+                finalPosition=index;
+            }
+        }
+        return finalPosition==0;  
+    }
+}
