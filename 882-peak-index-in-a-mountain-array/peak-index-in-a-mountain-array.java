9d9c85f8a302a398b9b89f69df827cd02b9c7910
Chiranthstack <chiranth.s2005@gmail.com>
2026-05-06 23:44:18 +0530

    Time: 0 ms (100.00%) | Memory: 80.7 MB (7.64%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
@@ -4,13 +4,13 @@ class Solution {
         int res=-1;
         while(low<=high){
             int guess=low+(high-low)/2;
-            if(arr[guess]<arr[guess+1]){
+            if (arr[guess]<arr[guess+1]){
                 low=guess+1;
             } else {
                 res=guess;
                 high=guess-1;
             }
         }
-        return res; 
+        return res;   
     }
 }
