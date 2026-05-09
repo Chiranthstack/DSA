dbdef3aa99e903f1db620549d12685e3c49ab4c0
Chiranthstack <chiranth.s2005@gmail.com>
2026-05-09 10:36:08 +0530

    Time: 2 ms (95.20%) | Memory: 71.8 MB (58.50%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,17 +1,17 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int n=nums.length;
-        int[] ans= new int[n];
-        ans[0]=1;
+        int[] answer=new int[n];
+        answer[0]=1;
         for(int i=1;i<n;i++){
-            ans[i]=ans[i-1]*nums[i-1];
-
+            answer[i]=answer[i-1]*nums[i-1];
         }
         int suffix=1;
         for(int i=n-1;i>=0;i--){
-            ans[i]=ans[i]*suffix;
-            suffix*=nums[i];        }
+            answer[i]=answer[i]*suffix;
+            suffix*=nums[i];
+        }
 
-        return ans;
+        return answer;
     }
 }
