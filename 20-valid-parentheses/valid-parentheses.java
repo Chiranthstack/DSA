84b8c2af984c05210a6581164f80406b67957fc0
Chiranthstack <chiranth.s2005@gmail.com>
2026-05-25 08:11:23 +0530

    Time: 3 ms (88.01%) | Memory: 43.1 MB (84.21%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,24 +1,21 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
+        Stack<Character> stack=new Stack<>();
         for(int i=0;i<s.length();i++){
             char ch=s.charAt(i);
-        if(ch=='('){
-            stack.push(')');
-        } else if(ch=='{'){
-            stack.push('}');
-        } else if(ch=='['){
-            stack.push(']');
-        }
-        else{
-            if(stack.empty()||stack.pop()!=ch){
-                return false;
+            if(ch=='('){
+                stack.push(')');
+            } else if(ch=='{'){
+                stack.push('}');
+            } else if(ch=='['){
+                stack.push(']');
+            } else {
+                if(stack.empty()||stack.pop()!=ch){
+                    return false;
+                }
             }
         }
-        }
         return stack.isEmpty();
-
-    
         
     }
 }
